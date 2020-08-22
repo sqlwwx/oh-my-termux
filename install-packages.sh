d2c7244d814548d9c7c52e9e97781bc3bc37bdf0
sqlwwx <wwx@wuweixing.com>
2020-08-22 20:54:24 +0800

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt upgrade
-apt install -y vim openssh coreutils mosh termux-api tree nodejs-dev
+apt install -y vim openssh coreutils mosh termux-api tree nodejs
 npm install -g http-server
 mkdir $HOME/www
 ln -s /etc/hosts $HOME/www/
