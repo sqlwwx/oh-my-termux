8b7bdde88050520aa38e2f76447de2c222874a0d
sqlwwx <wwx@wuweixing.com>
2020-08-22 20:33:13 +0800

    chore: update

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo "deb [arch=all,aarch64] http://mirrors.tuna.tsinghua.edu.cn/termux stable main" > $HOME/../usr/etc/apt/sources.list
-pkg update
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
+sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
+sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
+apt update && apt upgrade
 pkg install -y git zsh curl
 clear
 
